bc289fb7ebd4fbe16661930dc5eeb4dec2299197
kn7767 <112599359+kn7767@users.noreply.github.com>
2022-10-19 10:01:55 -0700

    local storage created

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 var today = moment();
 $('#currentDay').text(today.format('LLLL'));
+
+renderInput();
+
+var saveInput = document.getElementById('save');
+var nineam = document.getElementById('nineam');
+
+function savetoStorage() {
+    let hournine = document.querySelector('#nine').value;
+    localStorage.setItem('nine', hournine);
+    console.log(hournine);
+}
+saveInput.addEventListener("click", savetoStorage);
+
+function renderInput() {
+var hournine = localStorage.getItem('nine');
+hournine.textContent = hournine;
+console.log(localStorage);
+}
+
+
+// var nineInput = document.getElementById('nineam');
+// function savetoStorage() {
+//     localStorage.setItem("nineam", nineInput);
+//     // nineInput.innerHTML =
+//     localStorage.getItem("nineam");
+//     console.log(nineInput);
+// }
+
+
+
+
+// function renderLastRegistered() {
+//     var nineam = localStorage.getItem('nineam');
+//     nineInput.textContent = nineam;
+// }
+
+//delete data - not required in proj
+var clearInputs = document.getElementById('clear');
+function clearStorage() {
+    localStorage.clear();
+    hournine.textContent = "";
+}
+clearInputs.addEventListener('click', clearStorage);
+
