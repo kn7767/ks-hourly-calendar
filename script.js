83af7e371d142339645ea07bc9f834b0b70815c4
kn7767 <112599359+kn7767@users.noreply.github.com>
2022-10-19 12:25:10 -0700

    prevent old data from clearing when page reloads

--- script.js
+++ script.js
@@ -7,41 +7,62 @@ var saveInput = document.getElementById('save');
 
 function savetoStorage() {
     let hournine = document.querySelector('#nine').value;
+    if (hournine != "") {
     localStorage.setItem('nine', hournine);
+    }
+    let hourten = document.querySelector('#ten').value;
+    if (hourten != "") {
+    localStorage.setItem('ten', hourten);
+    }
+    let houreleven = document.querySelector('#eleven').value;
+    if (houreleven !="") {
+    localStorage.setItem('eleven', houreleven);
+    }
+    let hourtwelve = document.querySelector('#twelve').value;
+    if (hourtwelve !=""){
+    localStorage.setItem('twelve', hourtwelve);
+    }
+    let hourone = document.querySelector('#one').value;
+    if (hourone != ""){
+    localStorage.setItem('one', hourone);
+    }
+    let hourtwo = document.querySelector('#two').value;
+    if (hourtwo != ""){
+    localStorage.setItem('two', hourtwo);
+    }
+    let hourthree = document.querySelector('#three').value;
+    if (hourthree !="") {
+    localStorage.setItem('three', hourthree);
+    }
+    let hourfour = document.querySelector('#four').value;
+    if (hourfour !=""){
+    localStorage.setItem('four', hourfour);
+    }
+    let hourfive = document.querySelector('#five').value;
+    if (hourfive !=""){
+    localStorage.setItem('five', hourfive);
+    }
 }
 saveInput.addEventListener("click", savetoStorage);
 
 function renderInput() {
-var hournine = localStorage.getItem('nine');
-hournine.textContent = hournine;
-console.log(hournine);
-
-console.log(localStorage);
-$("#nine .description").val(localStorage.getItem("nine"));
+let hournine = localStorage.getItem('nine');
+console.log("9:00AM = " + hournine);
+let hourten = localStorage.getItem('ten');
+console.log("10:00AM = " + hourten);
+var houreleven = localStorage.getItem('eleven');
+console.log("11:00AM = " + houreleven);
+var hourtwelve = localStorage.getItem('twelve');
+console.log("12:00 PM = " + hourtwelve);
+var hourone = localStorage.getItem('one');
+console.log("1:00PM = " + hourone);
+var hourtwo = localStorage.getItem('two');
+console.log("2:00PM = " + hourtwo);
+var hourthree = localStorage.getItem('three');
+console.log("3:00PM = " + hourthree);
+var hourfour = localStorage.getItem('four');
+console.log("4:00 PM = " + hourfour);
+var hourfive = localStorage.getItem('five');
+console.log("5:00 PM = " + hourfive);
 }
-
-
-// var nineInput = document.getElementById('nineam');
-// function savetoStorage() {
-//     localStorage.setItem("nineam", nineInput);
-//     // nineInput.innerHTML =
-//     localStorage.getItem("nineam");
-//     console.log(nineInput);
-// }
-
-
-
-
-// function renderLastRegistered() {
-//     var nineam = localStorage.getItem('nineam');
-//     nineInput.textContent = nineam;
-// }
-
-//delete data - not required in proj
-var clearInputs = document.getElementById('clear');
-function clearStorage() {
-    localStorage.clear();
-    hournine.textContent = "";
-}
-clearInputs.addEventListener('click', clearStorage);
 
